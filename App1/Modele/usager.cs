03716b98834f9aa3d50a88abc9db655a513f9c0b
Gabton Béton <gabton@outlook.com>
2017-03-29 17:33:12 -0400

    Update Usager et folders du projet

--- App1/Modele/usager.cs
+++ App1/Modele/usager.cs
@@ -12,8 +12,38 @@ using Android.Widget;
 
 namespace App1.Modele
 {
-    class usager
+    class Usager
     {
+        private string nom;
+        public string Nom { get; set; }
 
+        private string prenom;
+        public string Prenom { get; set; }
+
+        private List<Carte> listeCarte;
+        public int NombreCartes { get; set; }
+
+        private bool actif;
+        public bool Actif { get; set; }
+
+
+
+
+        public Usager(string nom, string prenom, int nombreCartes, bool actif)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.nombreCartes = nombreCartes;
+            this.actif = actif;
+        }
+
+        public void ActiverDesactiver()
+        {
+            if (this.actif)
+            {
+                this.actif = false;
+            }
+            else this.actif = true;
+        }
     }
 }
