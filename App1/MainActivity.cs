982a196e9d06aad4b37d1e9e6abb22baffaa9b31
charles <etudiant@EW14-11-CHAVAI>
2017-03-29 17:10:31 -0400

    salut

--- App1/MainActivity.cs
+++ App1/MainActivity.cs
@@ -10,7 +10,6 @@ namespace App1
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
             //Set our view from the "main" layout resource
            //// SetContentView (Resource.Layout.Main);
         }
