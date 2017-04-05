d629f4220163a40aee489b4da56058ebc7cf7299
SilverShroud <Utilisateur@WEB2016_13>
2017-04-05 16:19:21 -0400

    ListViewUsagerLayout

--- App1/Resources/Resource.Designer.cs
+++ App1/Resources/Resource.Designer.cs
@@ -60,65 +60,68 @@ namespace App1
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f05000e
-			public const int ContenuAdresse = 2131034126;
-			
-			// aapt resource value: 0x7f050010
-			public const int ContenuCourriel = 2131034128;
-			
-			// aapt resource value: 0x7f05000a
-			public const int ContenuNom = 2131034122;
-			
-			// aapt resource value: 0x7f05000c
-			public const int ContenuPrenom = 2131034124;
-			
-			// aapt resource value: 0x7f050012
-			public const int ContenuTelephone = 2131034130;
-			
-			// aapt resource value: 0x7f05000d
-			public const int LabelAdresse = 2131034125;
-			
 			// aapt resource value: 0x7f05000f
-			public const int LabelCourriel = 2131034127;
-			
-			// aapt resource value: 0x7f050009
-			public const int LabelNom = 2131034121;
-			
-			// aapt resource value: 0x7f05000b
-			public const int LabelPrenom = 2131034123;
+			public const int ContenuAdresse = 2131034127;
 			
 			// aapt resource value: 0x7f050011
-			public const int LabelTelephone = 2131034129;
+			public const int ContenuCourriel = 2131034129;
 			
-			// aapt resource value: 0x7f050002
-			public const int btnCartes = 2131034114;
+			// aapt resource value: 0x7f05000b
+			public const int ContenuNom = 2131034123;
 			
-			// aapt resource value: 0x7f050006
-			public const int btnGroupes = 2131034118;
-			
-			// aapt resource value: 0x7f050008
-			public const int btnPortes = 2131034120;
-			
-			// aapt resource value: 0x7f050004
-			public const int btnUsagers = 2131034116;
+			// aapt resource value: 0x7f05000d
+			public const int ContenuPrenom = 2131034125;
 			
 			// aapt resource value: 0x7f050013
-			public const int switchPorte = 2131034131;
+			public const int ContenuTelephone = 2131034131;
 			
-			// aapt resource value: 0x7f050000
-			public const int tableLayout1 = 2131034112;
+			// aapt resource value: 0x7f05000e
+			public const int LabelAdresse = 2131034126;
 			
-			// aapt resource value: 0x7f050001
-			public const int tableRow1 = 2131034113;
+			// aapt resource value: 0x7f050010
+			public const int LabelCourriel = 2131034128;
+			
+			// aapt resource value: 0x7f05000a
+			public const int LabelNom = 2131034122;
+			
+			// aapt resource value: 0x7f05000c
+			public const int LabelPrenom = 2131034124;
+			
+			// aapt resource value: 0x7f050012
+			public const int LabelTelephone = 2131034130;
 			
 			// aapt resource value: 0x7f050003
-			public const int tableRow2 = 2131034115;
-			
-			// aapt resource value: 0x7f050005
-			public const int tableRow3 = 2131034117;
+			public const int btnCartes = 2131034115;
 			
 			// aapt resource value: 0x7f050007
-			public const int tableRow4 = 2131034119;
+			public const int btnGroupes = 2131034119;
+			
+			// aapt resource value: 0x7f050009
+			public const int btnPortes = 2131034121;
+			
+			// aapt resource value: 0x7f050005
+			public const int btnUsagers = 2131034117;
+			
+			// aapt resource value: 0x7f050000
+			public const int listView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050014
+			public const int switchPorte = 2131034132;
+			
+			// aapt resource value: 0x7f050001
+			public const int tableLayout1 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int tableRow1 = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int tableRow2 = 2131034116;
+			
+			// aapt resource value: 0x7f050006
+			public const int tableRow3 = 2131034118;
+			
+			// aapt resource value: 0x7f050008
+			public const int tableRow4 = 2131034120;
 			
 			static Id()
 			{
@@ -134,10 +137,13 @@ namespace App1
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int ListViewUsager = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int VueUsager = 2130903041;
+			public const int Main = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int VueUsager = 2130903042;
 			
 			static Layout()
 			{
