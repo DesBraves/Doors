671b5e4202baf1d02e65862da2f8938178fc908d
Utilisateur <Utilisateur@EW15-02-GABMAR>
2017-04-07 14:13:07 -0400

    resource

--- App1/Resources/Resource.Designer.cs
+++ App1/Resources/Resource.Designer.cs
@@ -60,32 +60,32 @@ namespace App1
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050017
-			public const int ContenuAdresseUsager = 2131034135;
+			// aapt resource value: 0x7f05002e
+			public const int ContenuAdresseUsager = 2131034158;
 			
-			// aapt resource value: 0x7f050019
-			public const int ContenuCourrielUsager = 2131034137;
+			// aapt resource value: 0x7f050030
+			public const int ContenuCourrielUsager = 2131034160;
 			
-			// aapt resource value: 0x7f05000e
-			public const int ContenuDescriptionPorte = 2131034126;
+			// aapt resource value: 0x7f050025
+			public const int ContenuDescriptionPorte = 2131034149;
 			
-			// aapt resource value: 0x7f050010
-			public const int ContenuLocationPorte = 2131034128;
+			// aapt resource value: 0x7f050027
+			public const int ContenuLocationPorte = 2131034151;
 			
-			// aapt resource value: 0x7f05000c
-			public const int ContenuNom = 2131034124;
+			// aapt resource value: 0x7f050023
+			public const int ContenuNom = 2131034147;
 			
-			// aapt resource value: 0x7f050013
-			public const int ContenuNomUsager = 2131034131;
+			// aapt resource value: 0x7f05002a
+			public const int ContenuNomUsager = 2131034154;
 			
-			// aapt resource value: 0x7f050015
-			public const int ContenuPrenomUsager = 2131034133;
+			// aapt resource value: 0x7f05002c
+			public const int ContenuPrenomUsager = 2131034156;
+			
+			// aapt resource value: 0x7f050032
+			public const int ContenuTelephoneUsager = 2131034162;
 			
 			// aapt resource value: 0x7f05001b
-			public const int ContenuTelephoneUsager = 2131034139;
-			
-			// aapt resource value: 0x7f050016
-			public const int LabelAdresseUsager = 2131034134;
+			public const int ContenuUsager = 2131034139;
 			
 			// aapt resource value: 0x7f05002d
 			public const int LabelAdresseUsager = 2131034157;
@@ -121,64 +121,100 @@ namespace App1
 			public const int boutonModifier = 2131034135;
 			
 			// aapt resource value: 0x7f050018
-			public const int LabelCourrielUsager = 2131034136;
+			public const int boutonSupprimer = 2131034136;
 			
-			// aapt resource value: 0x7f05000d
-			public const int LabelDescriptionPorte = 2131034125;
+			// aapt resource value: 0x7f050021
+			public const int btnAfficherPorte = 2131034145;
 			
-			// aapt resource value: 0x7f05000f
-			public const int LabelLocationPorte = 2131034127;
-			
-			// aapt resource value: 0x7f05000b
-			public const int LabelNomPorte = 2131034123;
-			
-			// aapt resource value: 0x7f050012
-			public const int LabelNomUsager = 2131034130;
-			
-			// aapt resource value: 0x7f050014
-			public const int LabelPrenomUsager = 2131034132;
-			
-			// aapt resource value: 0x7f05001a
-			public const int LabelTelephoneUsager = 2131034138;
-			
-			// aapt resource value: 0x7f050001
-			public const int ListViewUsager = 2131034113;
-			
-			// aapt resource value: 0x7f050004
-			public const int btnCartes = 2131034116;
-			
-			// aapt resource value: 0x7f050008
-			public const int btnGroupes = 2131034120;
-			
-			// aapt resource value: 0x7f05000a
-			public const int btnPortes = 2131034122;
-			
-			// aapt resource value: 0x7f050006
-			public const int btnUsagers = 2131034118;
-			
-			// aapt resource value: 0x7f050000
-			public const int listViewUsager = 2131034112;
-			
-			// aapt resource value: 0x7f050011
-			public const int switchPorte = 2131034129;
+			// aapt resource value: 0x7f050020
+			public const int btnAfficherUsager = 2131034144;
 			
 			// aapt resource value: 0x7f05001c
-			public const int switchUsager = 2131034140;
+			public const int btnCartes = 2131034140;
 			
-			// aapt resource value: 0x7f050002
-			public const int tableLayout1 = 2131034114;
+			// aapt resource value: 0x7f05001e
+			public const int btnGroupes = 2131034142;
 			
-			// aapt resource value: 0x7f050003
-			public const int tableRow1 = 2131034115;
+			// aapt resource value: 0x7f05001f
+			public const int btnPortes = 2131034143;
 			
-			// aapt resource value: 0x7f050005
-			public const int tableRow2 = 2131034117;
+			// aapt resource value: 0x7f05001d
+			public const int btnUsagers = 2131034141;
+			
+			// aapt resource value: 0x7f050014
+			public const int listViewCartesUsager = 2131034132;
+			
+			// aapt resource value: 0x7f050013
+			public const int switchActifUsager = 2131034131;
+			
+			// aapt resource value: 0x7f050028
+			public const int switchPorte = 2131034152;
+			
+			// aapt resource value: 0x7f050033
+			public const int switchUsager = 2131034163;
+			
+			// aapt resource value: 0x7f050000
+			public const int tableLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f05000a
+			public const int tableLayout2 = 2131034122;
+			
+			// aapt resource value: 0x7f050015
+			public const int tableLayout3 = 2131034133;
+			
+			// aapt resource value: 0x7f050001
+			public const int tableRow1 = 2131034113;
+			
+			// aapt resource value: 0x7f050004
+			public const int tableRow2 = 2131034116;
 			
 			// aapt resource value: 0x7f050007
 			public const int tableRow3 = 2131034119;
 			
+			// aapt resource value: 0x7f05000b
+			public const int tableRow4 = 2131034123;
+			
+			// aapt resource value: 0x7f05000e
+			public const int tableRow5 = 2131034126;
+			
+			// aapt resource value: 0x7f050011
+			public const int tableRow6 = 2131034129;
+			
+			// aapt resource value: 0x7f050016
+			public const int tableRow7 = 2131034134;
+			
+			// aapt resource value: 0x7f050012
+			public const int tagActifUsager = 2131034130;
+			
+			// aapt resource value: 0x7f050008
+			public const int tagAdresseUsager = 2131034120;
+			
+			// aapt resource value: 0x7f05000c
+			public const int tagCourielUsager = 2131034124;
+			
+			// aapt resource value: 0x7f050002
+			public const int tagNomUsager = 2131034114;
+			
+			// aapt resource value: 0x7f050005
+			public const int tagPrenomUsager = 2131034117;
+			
+			// aapt resource value: 0x7f05000f
+			public const int tagTelephoneUsager = 2131034127;
+			
 			// aapt resource value: 0x7f050009
-			public const int tableRow4 = 2131034121;
+			public const int valueAdresseUsager = 2131034121;
+			
+			// aapt resource value: 0x7f05000d
+			public const int valueCourielUsager = 2131034125;
+			
+			// aapt resource value: 0x7f050003
+			public const int valueNomUsager = 2131034115;
+			
+			// aapt resource value: 0x7f050006
+			public const int valuePrenomUsager = 2131034118;
+			
+			// aapt resource value: 0x7f050010
+			public const int valueTelephoneUsager = 2131034128;
 			
 			static Id()
 			{
